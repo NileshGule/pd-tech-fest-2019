36176cf111d9f049cfe7cd6f943f3d689d1d3e8a
Nilesh Gule <vn_nilesh@yahoo.com>
2020-04-12 18:28:20 +0800

    Added Powershell script for deploying scaled object

---
+++ Powershell/deployAutoScaler.ps1
@@ -0,0 +1,12 @@
+# source common variables
+. .\var.ps1
+
+Write-Host "Starting deployment of ScaledObject and related resources to Kuberentes cluster" -ForegroundColor Yellow
+
+Write-Host "Deploying Tech Talks Consumer Autoscalar" -ForegroundColor Yellow
+Set-Location $sutoScalarRootDirectory
+kubectl apply --recursive --filename .
+
+Write-Host "ScaledObject and related resources deployed successfully" -ForegroundColor Cyan
+
+Set-Location ~/projects/pd-tech-fest-2019/Powershell
--- Powershell/deployKEDA.ps1
+++ Powershell/deployKEDA.ps1
@@ -6,16 +6,18 @@ helm repo update
 # Wait for 30 seconds for the tiller service to be available in the cluster
 # If tiller is not available, Keda installation fails
 # In case of Keda failure, the helm install command given below can be executed manually
-Start-Sleep -Seconds 30
+# Start-Sleep -Seconds 30
 
 Write-Host "Initializing KEDA on AKS cluster $clusterName" -ForegroundColor Green
 
 kubectl create namespace keda
 
+#Helm 3 syntax
 helm install keda `
     kedacore/keda `
     --namespace keda
 
+#Helm 2 syntax
 # helm install kedacore/keda-edge `
 #     --devel `
 #     --set logLevel=debug `
