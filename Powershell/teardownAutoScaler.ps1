56143abc98d53635f99918e0549e0bc986283385
Nilesh Gule <vn_nilesh@yahoo.com>
2020-05-09 23:47:07 +0800

    added teardown script for autoscalar

---
+++ Powershell/teardownAutoScaler.ps1
@@ -0,0 +1,12 @@
+# source common variables
+. .\var.ps1
+
+Write-Host "Starting deletion of ScaledObject and related resources from Kuberentes cluster" -ForegroundColor Yellow
+
+Write-Host "Deleting Tech Talks Consumer Autoscalar" -ForegroundColor Yellow
+Set-Location $sutoScalarRootDirectory
+kubectl delete --recursive --filename .
+
+Write-Host "ScaledObject and related resources deleted successfully" -ForegroundColor Cyan
+
+Set-Location ~/projects/pd-tech-fest-2019/Powershell
