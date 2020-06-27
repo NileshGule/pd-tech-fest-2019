89d21b325bba050878c30eefc69a2d7758195503
Nilesh Gule <vn_nilesh@yahoo.com>
2020-06-27 22:32:31 +0800

    Updated helm chart and password for Bitnami RabbitMQ

--- Powershell/deployRabbitMQ.ps1
+++ Powershell/deployRabbitMQ.ps1
@@ -1,8 +1,9 @@
 Write-Host "Starting deployment of RabbitMQ using Helm" -ForegroundColor Yellow
 
 helm install rabbitmq `
-    --set rabbitmq.username=user `
-    --set rabbitmq.password=PASSWORD `
-    azure-marketplace/rabbitmq
+    --set auth.username=user `
+    --set auth.password=PASSWORD `
+    bitnami/rabbitmq
+    # azure-marketplace/rabbitmq
 
 Write-Host "Deployment of RabbitMQ using Helm completed successfully" -ForegroundColor Yellow
