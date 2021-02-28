88ebc926f7437bdd5c7827408ba81c545502499c
Nilesh Gule <vn_nilesh@yahoo.com>
2021-02-28 18:33:27 +0800

    Added resources for helm chart first draft

--- Powershell/deployNginx.ps1
+++ Powershell/deployNginx.ps1
@@ -8,11 +8,12 @@ Write-Host "Initializing NGINX Ingress on AKS cluster $clusterName" -ForegroundC
 # kubectl create namespace ingress-basic
 
 # Use Helm to deploy an NGINX ingress controller
-helm install nginx-ingress ingress-nginx/ingress-nginx `
+helm upgrade nginx-ingress ingress-nginx/ingress-nginx `
     --namespace default `
     --set controller.replicaCount=2 `
     --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux `
     --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux `
-    --set controller.admissionWebhooks.patch.nodeSelector."beta\.kubernetes\.io/os"=linux
+    --set controller.admissionWebhooks.patch.nodeSelector."beta\.kubernetes\.io/os"=linux `
+    --set controller.customPorts="8085"
 
 Set-Location ~/projects/pd-tech-fest-2019/Powershell
