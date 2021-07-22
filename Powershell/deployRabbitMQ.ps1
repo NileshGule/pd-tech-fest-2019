dc1b81496e8cf7620d072f0d8229bdd6e3d2231b
Nilesh Gule <vn_nilesh@yahoo.com>
2021-07-22 08:03:17 +0800

    added erlang cookie to the parameters for rabbitmq helm chart

--- Powershell/deployRabbitMQ.ps1
+++ Powershell/deployRabbitMQ.ps1
@@ -6,6 +6,7 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 helm install rabbitmq `
+    --version 8.15.2 `
     --set auth.username=user `
     --set auth.password=PASSWORD `
     --set auth.erlangCookie=c2VjcmV0Y29va2ll `
