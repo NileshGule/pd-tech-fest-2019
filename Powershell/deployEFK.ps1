453320306e04559aa65c84d2c2f61b9f6493183b
Nilesh Gule <vn_nilesh@yahoo.com>
2022-01-28 19:33:25 +0800

    code cleanup after .Net 6.0 upgrade

--- Powershell/deployEFK.ps1
+++ Powershell/deployEFK.ps1
@@ -1,38 +1,21 @@
 Write-Host "Starting deployment of Elasticsearch using Helm" -ForegroundColor Yellow
 
-# helm repo add bitnami https://charts.bitnami.com/bitnami
-# helm repo add stable https://charts.helm.sh/stable
 helm repo add elastic https://helm.elastic.co
 
 helm repo update
 
 helm install elasticsearch --version 7.13 elastic/elasticsearch
 
-# helm upgrade --install efk `
-#     --set global.kibanaEnabled=true `
-#     bitnami/elasticsearch
-    
 Write-Host "Deployment of Elasticsearch using Helm completed successfully" -ForegroundColor Yellow
 
 Write-Host "Starting deployment of Fluent-bit using Helm" -ForegroundColor Yellow
-
-# helm upgrade --install fluentd `
-#     bitnami/fluentd
 
 helm repo add fluent https://fluent.github.io/helm-charts
 
 helm install fluent-bit fluent/fluent-bit `
 --values ../k8s/EFK/fluentbit-deamonset.yaml
 
-# helm install fluent-bit fluent/fluent-bit `
-#     --values fluentbit-deamonset.yaml
-
 Write-Host "Deployment of Fluent-bit using Helm completed successfully" -ForegroundColor Yellow
-
-#kubectl port-forward --namespace default svc/efk-coordinating-only 9200:9200
-#kubectl get all -l "app.kubernetes.io/name=fluentd,app.kubernetes.io/instance=fluentd"
-# kubectl port-forward --namespace default svc/kibana 5601:5601
-
 # kubectl apply -f https://github.com/fluent/fluentd-kubernetes-daemonset/blob/master/fluentd-daemonset-elasticsearch.yaml
 
 Write-Host "Starting deployment of Kibana using Helm" -ForegroundColor Yellow
